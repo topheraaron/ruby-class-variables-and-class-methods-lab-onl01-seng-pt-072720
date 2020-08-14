29c7d11e59515a8fb1a1ec1fa30660124647263b
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-14 23:29:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,17 +2,17 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  @@song_count = 0
+  @@count = 0
 
   def initialize(song_name, artist, genre)
     @name = song_name
     @artist = artist
     @genre = genre
-    @@song_count += 1
+    @@count += 1
 
   end
 
   def self.count
-    @song_count
+    @count
   end
 end
